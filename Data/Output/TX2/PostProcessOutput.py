8ac76c98eeff106882ad7ac9340df0de67134cc7
iqbal128855 <mdshahriariqbal@knights.ucf.edu>
2019-06-13 12:27:54 -0400

    Added data with no-intervention and intervention with context switches

--- Data/Output/TX2/PostProcessOutput.py
+++ Data/Output/TX2/PostProcessOutput.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import math
 
 def read_data(fn):
     """This function is used to read data as a dataframe
     """
     return pd.read_csv(fn)
 
-def find_dup(df):
+def process_df(df):
     """This function is used to find duplicates and process them for averaging
     """
     dup=df[df.duplicated(['core0_status',
@@ -22,6 +23,7 @@ def find_dup(df):
     cur=0
     ind=[x[0] for x in dup.values]
     fpr={}
+    # find duplicates
     for i in xrange(len(ind)-1):
         if ind[i+1]-ind[i]==1:
             try:
@@ -30,26 +32,37 @@ def find_dup(df):
                 fpr[cur]=[ind[i]]
         else:
             fpr[cur].append(ind[i])
-            if i<len(ind)-1:
-                cur=ind[i+1]-1
-                fpr[cur]=[ind[i+1]]
-    print fpr, ind    
-            
-        
+            cur=ind[i+1]-1
     
+    # perform average            
+    new_df_val=[]
+    df_val=list(df.values)
+    for k,v in fpr.iteritems():
+        cur=df_val[int(k)]
+        counter=1
+        for i in xrange(len(v)):
+            cur=[sum(x) for x in zip(cur,df_val[int(v[i])])]
+            counter=counter+1
+        final=[x/counter for x in cur]
+        new_df_val.append(final)   
+    
+    cols=df.columns
+    new_df=pd.DataFrame(new_df_val)
+    new_df.columns=cols
+    return new_df
+               
 def save_file(fn,
               df):
     """This file is used to save file
     """
-    df.to_csv('post_processed_'+fn)
+    df.to_csv('post_'+fn)
 
 if __name__=="__main__":
-   #fn=raw_input("Enter file name: ")
-   fn='processed_output_no_intervention.csv'
+   
+   fn=raw_input("Enter file name: ")
    df=read_data(fn)
    df.dropna()
-   find_dup(df)
-   
-   #save_file(fn,df)
+   new_df=process_df(df) 
+   save_file(fn,new_df)
    
    
