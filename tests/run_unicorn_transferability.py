011cae971a7ab9273fc2e4c3538fbf6cd69e9f2f
iqbal128855 <mdshahriariqbal@knights.ucf.edu>
2022-02-02 19:10:05 -0500

    updated print utilitiy

--- tests/run_unicorn_transferability.py
+++ tests/run_unicorn_transferability.py
@@ -6,6 +6,7 @@ import json
 from ananke.graphs import ADMG
 from networkx import DiGraph
 from optparse import OptionParser
+import time
 
 sys.path.append('/root')
 
@@ -72,7 +73,7 @@ if __name__ == "__main__":
     perf_columns = cfg["perf_columns"]
 
     obj_columns = options.obj
-    columns = soft_columns + hw_columns + kernel_columns + obj_columns + perf_columns
+    columns = soft_columns + hw_columns + kernel_columns + perf_columns + obj_columns
     conf_opt = soft_columns + hw_columns + kernel_columns
 
     if len(options.obj) > 1:
@@ -82,7 +83,7 @@ if __name__ == "__main__":
                                options.software, options.hardware + "_" + options.software + "_" + "multi.csv")
         target_bug_dir = os.path.join(os.getcwd(), cfg["bug_dir"], "multi",
                                       target_hw, options.software, target_hw + "_" + options.software + "_" + options.obj[0] + ".csv")
-
+        
         with open(os.path.join(os.getcwd(), cfg["debug_dir"], "multi", target_hw,
                   options.software, "measurement_t.json")) as mfl:
             m = json.load(mfl)
@@ -90,6 +91,8 @@ if __name__ == "__main__":
 
         init_dir = os.path.join(os.getcwd(), cfg["init_dir"], "single",
                                 options.hardware, options.software, options.hardware + "_" + options.software + "_" + "initial.csv")
+        target_init_dir = os.path.join(os.getcwd(), cfg["init_dir"], "multi",
+                                target_hw, options.software, target_hw + "_" + options.software + "_" + "initial.csv")
         bug_dir = os.path.join(os.getcwd(), cfg["bug_dir"], "single",
                                options.hardware, options.software, options.hardware + "_" + options.software + "_" + options.obj[0] + ".csv")
         target_bug_dir = os.path.join(os.getcwd(), cfg["bug_dir"], "single",
@@ -98,6 +101,9 @@ if __name__ == "__main__":
         with open(os.path.join(os.getcwd(), cfg["debug_dir"], "single", target_hw,
                   options.software, "measurement_t.json")) as mfl:
             m = json.load(mfl)
+        with open(os.path.join(os.getcwd(), cfg["debug_dir"], "single", target_hw,
+                  options.software, "measurement.json")) as mfl:
+            mt = json.load(mfl)
     # get init data
     df = pd.read_csv(init_dir)
     df = df[columns]
@@ -122,9 +128,14 @@ if __name__ == "__main__":
     # get bug data from the target
     bug_df = pd.read_csv(target_bug_dir)
     # get Bug and update df
-
+    
     # UNICORN + 25
+    start = time.time()
     for bug_id in range(len(bug_df)):
+        # initialize dataframe after each run
+        df = pd.read_csv(init_dir)
+        df = df[columns]
+        
         bug = bug_df.loc[bug_id]
         num_samples = 0
         bug_exists = True
@@ -168,13 +179,17 @@ if __name__ == "__main__":
                     print("Resolved BUG_ID", bug_id)
 
                     print("----------------Recommended Fix-------------------")
+                    print("----------------Unicorn + 25-------------------")
                     print(config)
+                    print ("Gain", (bug[options.obj[0]]-curm)/bug[options.obj[0]])
+                    print ("Number of samples", num_samples)
                     print("--------------------------------------------------")
-
+                    
                     print("-----------------------Bug------------------------")
                     print(bug[conf_opt])
+                    
                     print("--------------------------------------------------")
-
+                      
                 else:
                     curc = m[target_hw][options.software][options.obj[0]
                                                           ][str(bug_id)][str(num_samples)]["conf"]
@@ -213,3 +228,113 @@ if __name__ == "__main__":
                                      columns, options, NUM_PATHS)
             else:
                 print("[ERROR]: invalid mode")
+        break
+    # put output to a dataframe
+    result_df = output.copy()
+    result_df["method"]="Unicorn +25"
+    # Unicorn rerun
+    for bug_id in range(len(bug_df)):
+        # initialize dataframe after each run
+        df = pd.read_csv(target_init_dir)
+        df = df[columns]
+        
+        bug = bug_df.loc[bug_id]
+        num_samples = 0
+        bug_exists = True
+        print("--------------------------------------------------")
+        print("BUG ID: ", bug_id)
+        print("---------------------------------------------------")
+        previous_config = bug[conf_opt].copy()
+
+        while bug_exists:
+            # identify causal paths
+            paths = CM.get_causal_paths(columns, di_edges, bi_edges,
+                                        options.obj)
+            # compute causal paths
+            if len(options.obj) < 2:
+                # single objective faults
+                for key, val in paths.items():
+                    if len(paths[key]) > NUM_PATHS:
+                        paths = CM.compute_path_causal_effect(
+                            df, paths[key], G, NUM_PATHS)
+                    else:
+                        paths = paths[options.obj[0]]
+                # compute individual treatment effect in a path
+                config = CM.compute_individual_treatment_effect(df, paths, g,
+                                                                query, options, bug[options.obj[0]],
+                                                                previous_config, cfg, var_types)
+            else:
+                # multi objective faults
+                paths = paths[options.obj[0]]
+                # compute individual treatment effect in a path
+                config = CM.compute_individual_treatment_effect(df, paths, g,
+                                                                query, options, bug[options.obj],
+                                                                previous_config, cfg, var_types)
+
+            # perform intervention. This updates the init_data
+
+            if options.mode == "offline":
+                curm = mt[target_hw][options.software][options.obj[0]
+                                                      ][str(bug_id)][str(num_samples)]["measurement"]
+                if curm < (1 - query) * bug[options.obj[0]]:
+                    bug_exists = False
+                    print("Resolved BUG_ID", bug_id)
+
+                    print("----------------Recommended Fix-------------------")
+                    print("----------------Unicorn Rerun-------------------")
+                    print(config)
+                    print ("Gain", (bug[options.obj[0]]-curm)/bug[options.obj[0]])
+                    print ("Number of samples", num_samples)
+                    print("--------------------------------------------------")
+                    
+                    print("-----------------------Bug------------------------")
+                    print(bug[conf_opt])
+                    
+                    print("--------------------------------------------------")
+                      
+                else:
+                    curc = mt[target_hw][options.software][options.obj[0]
+                                                          ][str(bug_id)][str(num_samples)]["conf"]
+                    num_samples += 1
+
+                    output = config.tolist()
+                    output.extend(curc)
+                    output.extend([curm])
+                    output = pd.DataFrame([output])
+                    output.columns = columns
+                    df = pd.concat([df, output], axis=0)
+                    df = df[columns]
+                    # previous_config
+                    previous_config = output.squeeze()[conf_opt]
+                    # update initial
+                    run_unicorn_loop(CM, df, tabu_edges,
+                                     columns, options, NUM_PATHS)
+
+            elif options.mode == "online":
+                gprm = GenerateParams(cfg, options.software, "unicorn")
+                output = gprm.run_unicorn_experiment(config)
+                if output[options.obj[0]] < (1 - query) * bug_val:
+                    bug_exists = False
+                else:
+                    # run loop
+                    output = output.tolist()
+                    output.extend(curc)
+                    output.extend([curm])
+                    output = pd.DataFrame([output])
+                    output.columns = columns
+                    df = pd.concat([df, config], axis=0)
+                    df = df[columns]
+                    # previous_config
+                    previous_config = output.squeeze()[conf_opt]
+                    run_unicorn_loop(CM, df, tabu_edges,
+                                     columns, options, NUM_PATHS)
+            else:
+                print("[ERROR]: invalid mode")
+        break
+    end = time.time() - start
+    
+    output["method"]="Unicorn (Rerun)"
+    result_df = pd.concat([result_df,output],axis=0)
+    measurement_dir = os.path.join(os.getcwd(),"data","measurement","output","transfer_exp.csv")
+    result_df.to_csv()
+    print ("Total Experiment time", end)
